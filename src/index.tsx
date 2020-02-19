6b2a400e9b2d1fffaf787b33f34ad00684b4dd67
massimiliano gobbi <massimiliano.gobbi@gmail.com>
2020-02-19 10:38:04 +0100

    added eslint support

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import * as serviceWorker from './serviceWorker';
 import Home from './features/home/home';
 import { Provider } from 'react-redux';
 import { Route } from 'react-router';
@@ -28,8 +27,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
